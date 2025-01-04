74854c5256ad27d4414c59eac1ab8b6ef68c926d
John T. Wodder II <git@varonathe.org>
2025-01-04 22:55:27 +0000

    v2025.1.4 — Initial release

--- src/logsdb/__init__.py
+++ src/logsdb/__init__.py
@@ -4,7 +4,7 @@ Record incoming server activity in a database
 Visit <https://github.com/jwodder/logsdb> for more information.
 """
 
-__version__ = "0.1.0.dev1"
+__version__ = "2025.1.4"
 __author__ = "John Thorvald Wodder II"
 __author_email__ = "jwodder-logsdb@varonathe.org"
 __license__ = "MIT"
