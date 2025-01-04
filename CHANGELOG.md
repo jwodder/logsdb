1781f62088869c83b080f93e6ddebc04a7e35600
John T. Wodder II <git@varonathe.org>
2025-01-04 22:58:29 +0000

    Use versioningit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+In Development
+--------------
+- Use `versioningit` for versioning
+
+v2025.1.4
+---------
+Initial release
--- src/logsdb/__init__.py
+++ src/logsdb/__init__.py
@@ -4,7 +4,9 @@ Record incoming server activity in a database
 Visit <https://github.com/jwodder/logsdb> for more information.
 """
 
-__version__ = "2025.1.4"
+from importlib.metadata import version
+
+__version__ = version("jwodder-logsdb")
 __author__ = "John Thorvald Wodder II"
 __author_email__ = "jwodder-logsdb@varonathe.org"
 __license__ = "MIT"
